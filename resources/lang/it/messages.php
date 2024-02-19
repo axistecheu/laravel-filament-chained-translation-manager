1ccd816f40f3e00a27d19653f09cd3834768e304
Axis Technologies <94626178+axistecheu@users.noreply.github.com>
2024-02-19 09:24:39 +0000

    italian language added

---
+++ resources/lang/it/messages.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    'title' => 'Traduzioni',
+    'navigation-group' => 'Impostazioni',
+    'search_term_placeholder' => 'Cerca traduzione',
+    'selected_groups_placeholder' => 'Seleziona gruppo',
+    'selected_languages_placeholder' => 'Seleziona lingua',
+    'only_show_missing_translations_lbl' => 'Mostra solo traduzioni mancanti',
+    'error_no_translations_for_filters' => 'Modifica i filtri, non ci sono traduzioni corrispondenti alla tua query!',
+    'error_no_translation_loaded' => 'Non sono state trovate traduzioni. Controlla la tua directory <pre>lang</pre>',
+    'previous_page' => 'Precedente',
+    'next_page' => 'Successivo',
+    'missing_translation' => 'Traduzione non compilata',
+    'filter_action' => 'Filtro',
+    'filter_results' => ':filtered su :total traduzioni filtrate.',
+    'filter_results_missing_translations' => ':missing hanno traduzioni mancanti (:percent%).'
+];
